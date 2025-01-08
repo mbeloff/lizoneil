64384b43b0f70ff0aa3e4673950572672f0d6185
Michael Beloff <38679121+mbeloff@users.noreply.github.com>
2025-01-09 09:26:46 +1000

    -

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="text-sky-950">
-        <div class="p-3 flex justify-between fixed z-50 w-full bg-white">
+        <div class="p-4 flex justify-between fixed z-50 w-full bg-white items-center">
             <div>
                 <p class="font-bold text-sky-950 text-4xl font-sans"><span class="relative after:absolute after:content-[''] after:h-1.5 after:w-full after:bg-sky-500 after:bottom-0 after:left-0">M</span>yname</p>
             <p class="text-sky-400 mt-3 font-light">my city</p>
             </div>
-            <button><Icon name="material-symbols:menu" size="2em"></Icon></button>
+            <!-- <button><Icon name="material-symbols:menu" size="2em"></Icon></button> -->
+             <div class="invisible md:visible flex gap-4 items-center h-full">
+                <a class="font-bold flex items-center gap-2 text-xl hover:text-sky-500" href="tel:1234123123"><Icon name="material-symbols:phone-enabled" ></Icon> 1234 123 123</a> 
+                <a href="mailto:myname@mydomain.com" class="bg-sky-500 text-white hover:bg-sky-600 px-8 py-4 rounded flex items-center justify-center gap-2"><Icon name="material-symbols:mail" ></Icon>Enquire</a>
+            </div>
         </div>
-        <div class="bg-gray-200 flex flex-col min-h-screen bg-img bg-1">
-            <div class="mt-auto p-6 ">
-                <div class="text-white drop-shadow">
-                   <p class="text-4xl font-medium">My Name</p>
-                <p class="text-xl font-medium  mt-3">Focused on Clients,</p>
-                <p class="text-xl font-light">Driven by Results.</p>
-                <div class="flex text-sm font-light text mt-10 gap-2"><span>Licensed Sales Consultant</span><span>|</span><span>Some Company</span></div> 
-                </div>
-                
-
-                <div class="grid grid-cols-2 gap-2 mt-6 max-w-[350px]">
-                    <button class="bg-sky-500 text-white px-4 py-4 rounded flex items-center justify-center gap-2"><Icon name="material-symbols:phone-enabled" ></Icon>Click to call</button><button class="bg-white px-4 py-4 rounded flex items-center justify-center gap-2"><Icon name="material-symbols:mail" ></Icon>Enquire</button><button class="bg-white px-4 py-4 rounded col-span-full">Request Appraisal</button>
+        <div class="bg-1 bg-[25%] lg:bg-center bg-cover bg-1">
+            <div class="flex flex-col min-h-screen  lg:grid lg:place-items-center max-w-screen-lg mx-auto">
+                <div class="mt-auto lg:my-auto p-6 lg:ml-auto">
+                    <div class="text-white my-drop-shadow">
+                       <p class="text-4xl lg:text-6xl font-medium">My Name</p>
+                    <p class="text-xl lg:text-2xl font-medium  mt-3">Focused on Clients,</p>
+                    <p class="text-xl lg:text-2xl font-light">Driven by Results.</p>
+                    <div class="flex text-sm lg:text-base font-light text mt-10 gap-2"><span>Licensed Sales Consultant</span><span>|</span><span>Some Company</span></div> 
+                    </div>
+                    
+    
+                    <div class="grid grid-cols-2 gap-2 mt-6 w-full max-w-[350px] lg:max-w-[500px] text-center">
+                        <a href="tel:1234123123" class="bg-sky-500 hover:bg-sky-600 text-white px- py-4 rounded flex items-center justify-center gap-2"><Icon name="material-symbols:phone-enabled" ></Icon>Click to call</a>
+                        <a href="mailto:myname@mydomain.com" class="bg-white hover:bg-gray-200 px-4 py-4 rounded flex items-center justify-center gap-2"><Icon name="material-symbols:mail" ></Icon>Enquire</a>
+                        <a href="mailto:myname@mydomain.com" class="bg-white hover:bg-gray-200 px- py-4 rounded col-span-full ">Request Appraisal</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -29,13 +37,13 @@
                 <p>87 <br> <span class="text-sm">Properties Sold</span></p>
             </div>
         </div>
-        <div class="min-h-screen relative bg-img bg-2">
+        <div class="min-h-screen relative bg-center bg-cover bg-2">
             <div class="bg-black/50 absolute inset-0  text-center text-balance  py-40 px-5 grid place-items-center">
-                <div>
+                <div class="max-w-xl">
                     <p class="text-4xl leading-loose text-white"><span>What is your property</span><br><span class="font-handwritten text-sky-500 text-6xl">Worth?</span></p>
                 <p class="text-xl text-white"><span class="font-bold">Looking to buy or sell a property?</span> Get in touch for a property appraisal highlighting the market value including recent sales, rental history, suburb report and more.</p>
                 <div class="mt-10 flex justify-center">
-                    <button class="bg-sky-500 text-white px-4 py-4 md:py-4 rounded min-w-max">Get Property Appraisal</button>
+                    <a href="mailto:myname@mydomain.com" class="bg-sky-500 hover:bg-sky-600 text-white px-4 py-4 md:py-4 rounded min-w-max text-center">Get Property Appraisal</a>
                 </div>
                 </div>
                 
@@ -64,15 +72,18 @@
 /* @theme {
     --font-display: "Fira Sans", "sans-serif"
 } */
-.bg-img {
+/* .bg-center bg-cover {
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover
-}
+} */
 .bg-1 {
-    background-image: url('https://picsum.photos/seed/redrocket1/400/710')
+    background-image: url('https://ik.imagekit.io/y3ayiqneqlr/misc/stock01.jpg?updatedAt=1736376865753')
 }
 .bg-2 {
-    background-image: url('https://picsum.photos/seed/family/400/710')
+    background-image: url('https://ik.imagekit.io/y3ayiqneqlr/misc/stock02.jpg?updatedAt=1736376488255')
+}
+.my-drop-shadow {
+    filter: drop-shadow(0px 0px 4px #000000ad);
 }
 </style>
